cac6273d6f74b78d65bc6e04455adfd2812c0134
guangjiangwei <327214842@qq.com>
2019-11-30 15:57:06 +0800

    3个线程卖100张票

---
+++ SSM/src/cn/sunline/thread/SaleTickets.java
@@ -0,0 +1,34 @@
+package cn.sunline.thread;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Tickets {
+	
+	private volatile int tickets = 100;
+	private Lock lock = new ReentrantLock();
+	
+	public void saleTickets() {
+		lock.lock();
+		try {
+			if ( tickets > 0) {
+				System.out.println(Thread.currentThread().getName() + "卖出第" + (100 - --tickets) + "张票");
+			}
+		} catch (Exception e) {
+			
+		} finally {
+			lock.unlock();
+		}
+	}
+	
+}
+
+public class SaleTickets {
+
+	public static void main(String[] args) {
+		Tickets tickets = new Tickets();
+		new Thread(() -> {for (int i = 1; i <= 100; i++) tickets.saleTickets();},"线程A").start();
+		new Thread(() -> {for (int i = 1; i <= 100; i++) tickets.saleTickets();},"线程B").start();
+		new Thread(() -> {for (int i = 1; i <= 100; i++) tickets.saleTickets();},"线程C").start();
+	}
+}
